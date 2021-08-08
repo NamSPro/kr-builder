72d976f54369d46db5f01a5056716835b4c1c5ad
NamSPro <namhamhap2812@gmail.com>
2021-08-08 15:25:37 +0700

    add TODO

--- lib/gear.h
+++ lib/gear.h
@@ -34,7 +34,27 @@ enum GEAR_CONDITION{
 	GEAR_TIER7,
 	GEAR_TIER8,
 	GEAR_UNIQUE,
-	GEAR_TM
+	GEAR_TM,
+	GEAR_TM_RECLAIMED
+};
+
+enum GEAR_SET{
+	GEAR_SET_NONE,
+	GEAR_RED,
+	GEAR_ICE,
+	GEAR_POISON,
+	GEAR_BLACK,
+	GEAR_LAVA,
+	GEAR_LEGEND,
+	GEAR_DL,
+	GEAR_SUPPRESS,
+	GEAR_PROTECT,
+	GEAR_MANTI,
+	GEAR_TM_AMP,
+	GEAR_GALGORIA,
+	GEAR_SIEGFRIED,
+	GEAR_ASCALON,
+	GEAR_SET_TOTAL
 };
 
 // STAR_LEVEL enum
@@ -75,17 +95,20 @@ private:
 	// denotes the position and accessory type
 	// generally accessoryType should be ACCESSORY_NONE unless gearType is GEAR_ACCESSORY
 	int gearType, accessoryType;
-	// GEAR_TIER(number), GEAR_UNIQUE or GEAR_TM
+
+	// GEAR_TIER(number), GEAR_UNIQUE or GEAR_TM/GEAR_TM_RECLAIMED
 	int gearCondition;
-	// isReclaimed should only be changed when gearCondition is GEAR_TM
-	bool isReclaimed;
 	int starLevel;
+
 	// this represents flat stats
 	int statBoost;
+
 	// this represents gear options
 	Option gearOptions[GEAR_OPTIONS_TOTAL];
+
 	// this represents runes (from 1 of a single stat rune on armors, upto 3 Velk runes on weapon)
 	Rune gearRunes[GEAR_RUNES_TOTAL];
+
 	// this represents enchantments (from 1 of traditional gear to 3 of TM)
 	Option gearEnchants[GEAR_ENCHANTS_TOTAL];
 };
